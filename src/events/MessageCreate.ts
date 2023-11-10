cf49aa1f2e1cc8c94bf11de36390fa1075c987e1
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2023-11-10 20:48:04 +0000

    Implemented a feature in which staff members can trigger the GPT for other users.

--- src/events/MessageCreate.ts
+++ src/events/MessageCreate.ts
@@ -1,4 +1,5 @@
 import type { ArgsOf, Client } from 'discordx';
+import type { Message } from 'discord.js';
 import { Discord, On } from 'discordx';
 import { EmbedBuilder } from 'discord.js';
 import { loadAssistant } from '../utils/Util.js';
@@ -28,22 +29,39 @@ export class MessageCreate {
         }
 
         // Regex to match content less than or equal to 100, and ends with a question mark
-        const regex = /^.{10,100}\?$/;
+        const regex = /^.{5,100}\?$/;
 
         // Respond to messages with a 15% chance if they end with a question mark and is less than 100 characters
         const chance = Math.random();
-        if (chance <= 0.15) {
+        if (chance <= 0.10) {
             if (regex.test(message.content)) {
                 await message.channel.sendTyping();
 
                 // Query AirRepsGPT
-                await runGPT();
+                await runGPT(message.content, message);
                 return;
             }
         }
 
         // Return if no one was mentioned or the user mentioned was NOT the bot.
         if (!message.mentions.users.size || !message.mentions.has(`${client.user?.id}`)) return;
+
+        // Staff feature, where pinging AirRepsGPT while replying to another user will trigger the GPT provider.
+        const staffRoles = process.env.StaffRoles?.split(',');
+        const isStaff = staffRoles?.some((roleID) => message.member?.roles.cache.has(roleID));
+        if (isStaff && message.reference) {
+            try {
+                const repliedMessage = await message.channel.messages.fetch(`${message.reference.messageId}`);
+
+                if (repliedMessage && !repliedMessage.author.bot) {
+                    await message.channel.sendTyping();
+                    await runGPT(repliedMessage.content, repliedMessage);
+                }
+                return;
+            } catch (e) {
+                console.error('Error fetching or processing the replied message:', e);
+            }
+        }
 
         await message.channel.sendTyping();
 
@@ -54,22 +72,22 @@ export class MessageCreate {
             },
         ]);
 
-        await runGPT();
+        await runGPT(message.content, message);
 
-        async function runGPT() {
+        async function runGPT(cnt: string, msg: Message) {
             try {
                 // Load the Assistant for the message content
-                const res = await loadAssistant(client, message, message.content);
+                const res = await loadAssistant(client, message, cnt);
 
                 // Reply with the Assistant's response
                 if (res) {
-                    await message.reply(res);
+                    await msg.reply(res);
                 } else {
-                    await message.reply({ embeds: [errorEmbed] });
+                    await msg.reply({ embeds: [errorEmbed] });
                 }
             } catch (e) {
                 // Send an error message and log the error
-                await message.reply({ embeds: [errorEmbed] });
+                await msg.reply({ embeds: [errorEmbed] });
                 console.error(e);
             }
         }

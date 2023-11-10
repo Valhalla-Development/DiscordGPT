bd533a32f7854a0863f25546b8c0e1acea4b5163
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2023-11-10 15:09:24 +0000

    "Prevent bot from self-reply; add DM handling for bot
    Logic has been added to prevent the bot from replying to itself or other bots, improving efficiency by unneeded actions. If 'messageCreate' event is triggered outside a server, the bot now instructs users to use the bot within the AirReps Discord server to offer better assistance."

--- src/events/MessageCreate.ts
+++ src/events/MessageCreate.ts
@@ -11,6 +11,19 @@ export class MessageCreate {
      */
     @On({ event: 'messageCreate' })
     async onMessage([message]: ArgsOf<'messageCreate'>, client: Client) {
+        // Return if the author is a bot, preventing the bot from replying to itself or other bots.
+        if (message.author.bot) return;
 
+        // Direct users to the Discord server if messageCreate is triggered outside a guild.
+        if (!message.guild) {
+            const embed = new EmbedBuilder().setColor('#e91e63').addFields([
+                {
+                    name: `**${client.user?.username}**`,
+                    value: 'To better assist you, please use our bot within the [AirReps Discord server](https://airreps.link/discord).\nHead over there for a seamless experience. See you on the server!',
+                },
+            ]);
+
+            await message.reply({ embeds: [embed] });
+        }
     }
 }

b658f40c78de8786576ced777b575f94858bbd1f
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2023-11-10 19:56:22 +0000

    This change accommodate the previous commit

--- src/events/MessageCreate.ts
+++ src/events/MessageCreate.ts
@@ -1,8 +1,7 @@
 import type { ArgsOf, Client } from 'discordx';
 import { Discord, On } from 'discordx';
 import { EmbedBuilder } from 'discord.js';
-import OpenAI from 'openai';
-import type { MessageContentText } from 'openai/resources/beta/threads';
+import { loadAssistant } from '../utils/Util.js';
 
 @Discord()
 export class MessageCreate {
@@ -31,87 +30,7 @@ export class MessageCreate {
         // Return if no one was mentioned or the user mentioned was NOT the bot.
         if (!message.mentions.users.size || !message.mentions.has(`${client.user?.id}`)) return;
 
-        /**
-         * Load Assistant function to query the OpenAI API for a response.
-         * @param query - The user query to be sent to the Assistant.
-         * @returns The response text from the Assistant.
-         */
-        async function loadAssistant(query: string): Promise<string | undefined> {
-            const str = query.replaceAll(/<@!?(\d+)>/g, '');
-
-            if (!str.length || str.length <= 10) {
-                return 'Please enter a valid query, with a minimum length of 10 characters.';
-            }
-
-            try {
-                const openai = new OpenAI({
-                    apiKey: process.env.OpenAiKey,
-                });
-
-                // Retrieve the Assistant information
-                const assistant = await openai.beta.assistants.retrieve(`${process.env.AssistantId}`);
-
-                // Create a new thread
-                const thread = await openai.beta.threads.create();
-
-                // Add a user message to the thread
-                await openai.beta.threads.messages.create(thread.id, {
-                    role: 'user',
-                    content: str,
-                });
-
-                // Create a run with the Assistant
-                const createRun = await openai.beta.threads.runs.create(thread.id, {
-                    assistant_id: assistant.id,
-                });
-
-                let retrieve = await openai.beta.threads.runs.retrieve(thread.id, createRun.id);
-
-                // Define a sleep function
-                const sleep = (ms: number): Promise<void> => new Promise<void>((resolve) => {
-                    setTimeout(resolve, ms);
-                });
-
-                console.log(`Queued query: ${str}`);
-
-                /**
-                 * Check the completion status of the query run.
-                 */
-                async function checkCompletion() {
-                    console.log(`Status: ${retrieve.status}`);
-                    if (retrieve.status !== 'completed') {
-                        await sleep(2000);
-                        retrieve = await openai.beta.threads.runs.retrieve(thread.id, createRun.id);
-                        await checkCompletion();
-                    }
-                }
-
-                await checkCompletion();
-
-                // Get the list of messages in the thread
-                const messages = await openai.beta.threads.messages.list(thread.id);
-
-                console.log('Completed query.');
-
-                // Extract text value from the Assistant's response
-                const textValue = (messages.data[0].content[0] as MessageContentText)?.text?.value;
-
-                return textValue;
-            } catch (error) {
-                // Handling errors
-                const errorEmbed = new EmbedBuilder().setColor('#EC645D').addFields([
-                    {
-                        name: `**${client.user?.username}**`,
-                        value: 'An error occurred, please report this to a member of our moderation team.',
-                    },
-                ]);
-
-                // Send an error message and log the error
-                await message.reply({ embeds: [errorEmbed] });
-                console.error(error);
-                return undefined;
-            }
-        }
+        await message.channel.sendTyping();
 
         const errorEmbed = new EmbedBuilder().setColor('#EC645D').addFields([
             {
@@ -122,7 +41,7 @@ export class MessageCreate {
 
         try {
             // Load the Assistant for the message content
-            const res = await loadAssistant(message.content);
+            const res = await loadAssistant(client, message, message.content);
 
             // Reply with the Assistant's response
             if (res) {

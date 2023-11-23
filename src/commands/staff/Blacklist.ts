62ec8bb166cdae43e1d2db8c00953706910805f2
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2023-11-23 14:53:38 +0000

    Update initial blacklist query count
    Changed the initial value for blacklist query count from 1 to 0 in the setGptQueryData function. This change ensures that the count will be accurate for newly blacklisted users, starting from 0 rather than 1.

--- src/commands/staff/Blacklist.ts
+++ src/commands/staff/Blacklist.ts
@@ -77,7 +77,7 @@ export class Blacklist {
             // Update the blacklist and send a success message.
             await setGptQueryData(
                 user.id,
-                getDb ? Number(getDb.totalQueries) : 1,
+                getDb ? Number(getDb.totalQueries) : 0,
                 Number(RateLimit),
                 Number(1),
                 false,

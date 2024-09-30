68842008b598eeb0ec843a6955fd42fb4060da67
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2024-09-30 13:41:39 +0100

    ✨ (MessageCreate.ts): introduce GPT processing function for handling direct messages and improve bot response logic for guild messages.

--- src/events/MessageCreate.ts
+++ src/events/MessageCreate.ts
@@ -17,20 +17,49 @@ export class MessageCreate {
         // Return if the author is a bot, preventing the bot from replying to itself or other bots.
         if (message.author.bot) return;
 
-        const replyMsg = process.env.ProjectSupportInvite
-            ? `[${client.user?.username} Discord server](${process.env.ProjectSupportInvite})`
-            : `**${client.user?.username} Discord server**`;
+        // Function to process GPT for a given content and user ID.
+        const processGPT = async (content: string, user: User, msg: Message) => {
+            await message.channel?.sendTyping();
 
-        // Direct users to the Discord server if messageCreate is triggered outside a guild.
+            const response = await runGPT(content, user);
+
+            if (typeof response === 'boolean' && response) {
+                return message.reply({ content: `You currently have an ongoing request. Please refrain from sending additional queries to avoid spamming ${client?.user}` });
+            }
+
+            if (response === content.replaceAll(/<@!?(\d+)>/g, '')) {
+                return message.reply({
+                    content: `An error occurred, please report this to a member of our moderation team.\n
+            ${codeBlock('js', 'Error: Response was equal to query.')}`,
+                });
+            }
+
+            if (Array.isArray(response)) {
+                const initialMessage = await msg.reply({ content: response[0] });
+                await initialMessage.reply({ content: response[1] });
+            } else if (typeof response === 'string') {
+                await msg.reply({ content: response });
+            }
+        };
+
+        // Handle DMs
         if (!message.guild) {
-            const embed = new EmbedBuilder().setColor('#EC645D').addFields([
-                {
-                    name: `**${client.user?.username}**`,
-                    value: `To better assist you, please use our bot within the ${replyMsg}.\nHead over there for a seamless experience. See you on the server!`,
-                },
-            ]);
+            if (process.env.EnableDirectMessages === 'true') {
+                await processGPT(message.content, message.author, message);
+            } else {
+                const replyMsg = process.env.ProjectSupportInvite
+                    ? `[${client.user?.username} Discord server](${process.env.ProjectSupportInvite})`
+                    : `**${client.user?.username} Discord server**`;
 
-            await message.reply({ embeds: [embed] });
+                const embed = new EmbedBuilder().setColor('#EC645D').addFields([
+                    {
+                        name: `**${client.user?.username}**`,
+                        value: `To better assist you, please use our bot within the ${replyMsg}.\nHead over there for a seamless experience. See you on the server!`,
+                    },
+                ]);
+
+                await message.reply({ embeds: [embed] });
+            }
             return;
         }
 
@@ -40,16 +69,11 @@ export class MessageCreate {
 
         // Function to check whether the bot should respond to the message.
         const shouldRespond = () => {
-            // Check if the channel is excluded
             const excludedChannels = process.env.ExcludedChannels?.split(',') ?? [];
-            if (excludedChannels.includes(message.channel.id)) {
-                return false;
-            }
+            if (excludedChannels.includes(message.channel.id)) return false;
 
-            // Check if the message contains a ping that isn't for the bot
             if (message.mentions.users.size > 0 && !message.mentions.users.has(client.user!.id)) return false;
 
-            // Check other conditions
             const chance = Math.random();
             const regex = /^.{5,100}\?$/;
             return (
@@ -60,42 +84,12 @@ export class MessageCreate {
             );
         };
 
-        // Function to process GPT for a given content and user ID.
-        const processGPT = async (content: string, user: User, msg: Message) => {
-            await message.channel?.sendTyping();
-
-            const response = await runGPT(content, user);
-
-            // If the response is boolean and true, then the user already has an ongoing query
-            if (typeof response === 'boolean' && response) {
-                return message.reply({ content: `You currently have an ongoing request. Please refrain from sending additional queries to avoid spamming ${client?.user}` });
-            }
-
-            if (response === content.replaceAll(/<@!?(\d+)>/g, '')) {
-                return message.reply({
-                    content: `An error occurred, please report this to a member of our moderation team.\n
-                ${codeBlock('js', 'Error: Response was equal to query.')}`,
-                });
-            }
-
-            // If response is an array of responses
-            if (Array.isArray(response)) {
-                // Edit the first message
-                const initialMessage = await msg.reply({ content: response[0] });
-                await initialMessage.reply({ content: response[1] });
-            } else if (typeof response === 'string') {
-                // If the response is a string, send a single message
-                await msg.reply({ content: response });
-            }
-        };
-
-        // Respond to the message if the conditions are met.
+        // Handle guild messages
         if (shouldRespond()) {
             await processGPT(message.content, message.author, message);
             return;
         }
 
-        // Process the message if it is a reply.
         if (message.reference) {
             try {
                 const repliedMessage = await message.channel.messages.fetch(`${message.reference.messageId}`);
@@ -112,7 +106,6 @@ export class MessageCreate {
                 console.error('Error fetching or processing the replied message:', e);
             }
         } else if (message.mentions.has(`${client.user?.id}`)) {
-            // Process the message if the bot is mentioned.
             await processGPT(message.content, message.author, message);
         }
     }
